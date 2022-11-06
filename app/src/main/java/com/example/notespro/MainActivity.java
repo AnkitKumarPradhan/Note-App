0061682214e9153ded4c9789307b7c9d1a15fd82
Chinmaya Kumar Biswal <chinmayakumarbiswal16045@gmail.com>
2022-11-06 13:33:31 +0530

    List note from Firebase to RecyclerView

--- app/src/main/java/com/example/notespro/MainActivity.java
+++ app/src/main/java/com/example/notespro/MainActivity.java
@@ -1,15 +1,25 @@
 package com.example.notespro;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.firestore.Query;
+
+import java.util.Queue;
 
 public class MainActivity extends AppCompatActivity {
 
     FloatingActionButton addNoteBtn;
+    RecyclerView recyclerView;
+    ImageButton menuBtn;
+    NoteAdapter noteAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +27,41 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         addNoteBtn=findViewById(R.id.add_note_btn);
+        recyclerView=findViewById(R.id.recyler_view);
+        menuBtn=findViewById(R.id.menu_btn);
 
         addNoteBtn.setOnClickListener((v)-> startActivity(new Intent(MainActivity.this,NoteDetailsActivity.class)));
+        menuBtn.setOnClickListener((v)->showMenu());
+        setupRecyclerView();
+    }
+    void showMenu(){
+
+    }
+    void setupRecyclerView(){
+        Query query=Utility.getCollectionReferanceForNotes().orderBy("timestamp",Query.Direction.DESCENDING);
+        FirestoreRecyclerOptions<Note> options=new FirestoreRecyclerOptions.Builder<Note>()
+                .setQuery(query,Note.class).build();
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        noteAdapter=new NoteAdapter(options,this);
+        recyclerView.setAdapter(noteAdapter);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        noteAdapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        noteAdapter.stopListening();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        noteAdapter.notifyDataSetChanged();
     }
 }
